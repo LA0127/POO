d0a04b6031d60f3440664de496f547b7bd8c797f
Arturo <la.contreras.h@edu.utc.mx>
2021-11-04 00:17:17 -0600

    EntradaTexto.java

---
+++ 02_Entrada_de_datos/EntradaTexto.java
@@ -0,0 +1,17 @@
+class EntradaTexto{
+    public static void main(String[] args){
+      //Tipo de dato, nombre de la variable
+        String nombre;
+      //ln es para dar un salto de linea  
+        System.out.println("Introduce tu nombre: ");
+        nombre= System.console().readLine();
+        System.out.println("Hola, " + nombre + " Vous êtes les bienvenus au programme");
+
+
+
+
+
+    }
+
+
+}
