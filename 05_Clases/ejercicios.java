4760a07611213ea2f819cd9dc0dd9e4dfc6f6a3a
Arturo <la.contreras.h@edu.utc.mx>
2021-11-20 21:56:56 -0600

    Luis

--- 05_Clases/ejercicios.java
+++ 05_Clases/ejercicios.java
@@ -141,14 +141,14 @@ class ejercicios{
                 }
                 System.out.println();
                 for(int i = 0; i<n-2; i++){
-                    System.out.print("*");
+                    System.out.print(" *");
                     for(int j = 0; j<n-2;j++){
-                        System.out.print(" ");
+                        System.out.print("  ");
                     }
-                    System.out.println("*");
+                    System.out.println(" *");
                 }
                 for(int i = 1; i<=n; i++){
-                    System.out.print("*");
+                    System.out.print(" *");
                 }
                 System.out.println();
                 
